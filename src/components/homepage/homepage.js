966fa775d4b595ef1b6dd35208a11af27d9bb745
saravana kumar <kmsaravanan1311@gmail.com>
2021-10-19 09:56:57 +0530

    pie chart

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './homepage.css';
 import { useAuth } from "../../contexts/useAuth";
+import { Pie } from 'react-chartjs-2';
 import 'react-date-range/dist/styles.css'; 
 import 'react-date-range/dist/theme/default.css'; 
 import { DateRangePickerComponent } from '@syncfusion/ej2-react-calendars';
@@ -40,6 +41,35 @@ export default function Homepage() {
 
     const { currentUser } = useAuth()
     console.log('user',currentUser)
+
+    const data = {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            width: 50,
+            height:50,
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+          },
+        ],
+      };
       return (            
             <div className='container-fluid'>
                 <div className="row">
@@ -146,6 +176,7 @@ export default function Homepage() {
                                     </select>
                                     </div>
                                     </div>
+                                    <div><Pie data={data} /></div>  
                                 </div>
                             </div>
                             </div>
