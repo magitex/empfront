c5035c39ba800669291ce2033d777e80c2c387a6
saravana kumar <kmsaravanan1311@gmail.com>
2021-10-19 16:05:23 +0530

    graph monthly

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React,{useState, useMemo,useEffect } from 'react';
 import './homepage.css';
 import { useAuth } from "../../contexts/useAuth";
-import { Pie } from 'react-chartjs-2';
+//import { Pie,Bar } from 'react-chartjs-2';
 import 'react-date-range/dist/styles.css'; 
 import 'react-date-range/dist/theme/default.css'; 
 import { DateRangePickerComponent } from '@syncfusion/ej2-react-calendars';
+import Highcharts from "highcharts/highstock";
+import HighchartsReact from "highcharts-react-official";
+import { isSameQuarter, isSameYear } from "date-fns";
+//import { getData } from "./data";
+import Helper from '../helpers/networks';
 import "../../../node_modules/@syncfusion/ej2-base/styles/material.css";
 import "../../../node_modules/@syncfusion/ej2-buttons/styles/material.css";
 import "../../../node_modules/@syncfusion/ej2-lists/styles/material.css";
@@ -12,64 +17,198 @@ import "../../../node_modules/@syncfusion/ej2-inputs/styles/material.css";
 import "../../../node_modules/@syncfusion/ej2-popups/styles/material.css";
 import "../../../node_modules/@syncfusion/ej2-react-calendars/styles/material.css";
 
-
-
-
 export default function Homepage() { 
-    
-    function ShowDateMonthly(){
-        var date = new Date();
-        var firstDay = new Date(date.getFullYear(), date.getMonth(),2).toISOString().slice(0, 10);
-        var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 1).toISOString().slice(0, 10);      
-        var monthly = firstDay +' to '+lastDay;
-        console.log("monthly",monthly);      
-    }
-    function ShowDateQuarterly(){    
-        var date = new Date();
-        var firstDay = new Date(date.getFullYear(), date.getMonth(),2).toISOString().slice(0, 10);
-        var lastDay = new Date(date.getFullYear(), date.getMonth() + 3, 1).toISOString().slice(0, 10);      
-        var quarterly = firstDay +' to '+lastDay;
-        console.log("quarterly",quarterly);     
-    }
-    function ShowDateAnnually(){      
-        var date = new Date();
-        var firstDay = new Date(date.getFullYear(),1).toISOString().slice(0, 7);
-        var lastDay = new Date(date.getFullYear()+1,1).toISOString().slice(0, 7);      
-        var annually = firstDay +' to '+lastDay;
-        console.log("annually",annually);   
-    }
+    const[value,setValue] = useState({
+        selectedValue:''        
+})
 
+  const[invoicelist,setinvoice] = useState([])
+  async function invoiceList() {       
+      let data;          
+      data= await Helper.invoiceData();
+      const tempeminvoice =data && data.data; 
+      setinvoice(tempeminvoice); 
+      console.log(data);
+  }
+  
+  useEffect(()=>{ 
+      invoiceList();
+  },[])
+  function toTimestamp(strDate){
+    var datum = Date.parse(strDate);
+    return datum/1000;
+ }
+
+  let activities = [];
+  invoicelist.map((invoice,key)=>(
+  //total=invoice.invoiceDetails.map(item => eval(item.totalamount)).reduce((prev, next) => prev + next);
+     activities.push([toTimestamp(invoice.invoicedate),invoice.invoiceDetails.map(item => eval(item.totalamount)).reduce((prev, next) => prev + next)]) 
+
+      ));
+    //   let activities = 
+    //     [
+    //         [946702800000, 261],
+    //         [949381200000, 251],
+    //         [951886800000, 282],
+    //         [954565200000, 289],
+    //         [957153600000, 259]
+        
+          
+    // ];
+  
+  console.log("activity",activities);
+  const [lineOptions, setLineOptions] = useState({
+    title: { text: "Time series data" },
+    xAxis: { type: "datetime" },
+    series: [
+      {
+        name: "foo",
+        type: "line",
+        data: activities
+      }
+    ]
+  });
+  const [pieOptions, setPieOptions] = useState({
+    title: { text: "Time series data" },
+    xAxis: { type: "datetime" },
+    series: [
+      {
+        name: "foo",
+        type: "pie",
+        data: activities
+      }
+    ]
+  });
+  const [barOptions, setBarOptions] = useState({
+    title: { text: "Time series data" },
+    xAxis: { type: "datetime" },
+    series: [
+      {
+        name: "foo",
+        type: "column",
+        data: activities
+      }
+    ]
+  });
+  const getProcessedData = (method) => {
+    const processedData = [];
+    const monthlyData = activities;
+
+    monthlyData.forEach((el, index) => {
+      if (
+        index === 0 ||
+        !method(new Date(el[0]), new Date(monthlyData[index - 1][0]))
+      ) {
+        processedData.push(el);
+      } else {
+        processedData[processedData.length - 1][1] += el[1];
+      }
+    });
+    console.log("month",processedData)
+    return processedData;
+  };
+
+  const yearlyData = () => getProcessedData(isSameYear);
+  const quarterlyData =() => getProcessedData(isSameQuarter);
+
+  const setData = (period) => {
+    const processedData =
+      period === "month"
+        ? activities
+        : period === "year"
+        ? yearlyData
+        : quarterlyData;
+
+        setLineOptions({
+      series: [
+        {
+          data: processedData
+        }
+      ]
+        });
+        setPieOptions({
+        series: [
+          {
+            data: processedData
+          }
+        ]
+        });
+         setBarOptions({
+        series: [
+          {
+            data: processedData
+          }
+        ]
+        });
+  };
+function handleInput(e){
+    const newvalue = {...value}
+    newvalue[e.target.name]=e.target.value;
+    setValue(newvalue) 
+}   
+    // function ShowDateMonthly(){
+    //     var date = new Date();
+    //     var firstDay = new Date(date.getFullYear(), date.getMonth(),2).toISOString().slice(0, 10);
+    //     var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 1).toISOString().slice(0, 10);      
+    //     var monthly = firstDay +' to '+lastDay;
+    //     console.log("monthly",monthly);      
+    // }
+    // function ShowDateQuarterly(){    
+    //     var date = new Date();
+    //     var firstDay = new Date(date.getFullYear(), date.getMonth(),2).toISOString().slice(0, 10);
+    //     var lastDay = new Date(date.getFullYear(), date.getMonth() + 3, 1).toISOString().slice(0, 10);      
+    //     var quarterly = firstDay +' to '+lastDay;
+    //     console.log("quarterly",quarterly);     
+    // }
+    // function ShowDateAnnually(){      
+    //     var date = new Date();
+    //     var firstDay = new Date(date.getFullYear(),1).toISOString().slice(0, 7);
+    //     var lastDay = new Date(date.getFullYear()+1,1).toISOString().slice(0, 7);      
+    //     var annually = firstDay +' to '+lastDay;
+    //     console.log("annually",annually);   
+    // }
+    function renderGraph(){
+        if(value.selectedValue === 'Line')
+           return   <HighchartsReact highcharts={Highcharts} options={lineOptions}/>
+        else if(value.selectedValue==='Bar')
+           return <HighchartsReact highcharts={Highcharts} options={barOptions}/>
+        else 
+             return<HighchartsReact highcharts={Highcharts} options={pieOptions} />
+
+     }
     const { currentUser } = useAuth()
     console.log('user',currentUser)
 
-    const data = {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-          {
-            label: '# of Votes',
-            width: 50,
-            height:50,
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      };
+    // const data = {
+    //     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    //     datasets: [
+    //       {
+    //         label: '# of Votes',
+    //         width: 50,
+    //         height:50,
+    //         data: [12, 19, 3, 5, 2, 3],
+    //         backgroundColor: [
+    //           'rgba(255, 99, 132, 0.2)',
+    //           'rgba(54, 162, 235, 0.2)',
+    //           'rgba(255, 206, 86, 0.2)',
+    //           'rgba(75, 192, 192, 0.2)',
+    //           'rgba(153, 102, 255, 0.2)',
+    //           'rgba(255, 159, 64, 0.2)',
+    //         ],
+    //         borderColor: [
+    //           'rgba(255, 99, 132, 1)',
+    //           'rgba(54, 162, 235, 1)',
+    //           'rgba(255, 206, 86, 1)',
+    //           'rgba(75, 192, 192, 1)',
+    //           'rgba(153, 102, 255, 1)',
+    //           'rgba(255, 159, 64, 1)',
+    //         ],
+    //         borderWidth: 1,
+    //       },
+    //     ],
+    //   };
+     
+
       return (            
             <div className='container-fluid'>
                 <div className="row">
@@ -77,9 +216,9 @@ export default function Homepage() {
                         <div className="page-title-box">
                             <div className="page-title-right">
                                 <form className="d-flex">                              
-                                <button type="button" className="btn btn-outline-secondary m-1" onClick={ShowDateMonthly}>Monthly</button>
-                                <button type="button" className="btn btn-outline-secondary m-1" onClick={ShowDateQuarterly}>Quarterly</button>
-                                <button type="button" className="btn btn-outline-secondary m-1" onClick={ShowDateAnnually}>Annually</button>
+                                <button type="button" className="btn btn-outline-secondary m-1" onClick={() => setData("month")}>Monthly</button>
+                                <button type="button" className="btn btn-outline-secondary m-1" onClick={() => setData("quarter")}>Quarterly</button>
+                                <button type="button" className="btn btn-outline-secondary m-1" onClick={() => setData("year")}>Annually</button>
                                 </form>
                             </div>
                             <h4 className="page-title">Dashboard</h4>
@@ -168,25 +307,24 @@ export default function Homepage() {
                                     </div>
                                     <div className="col">
                                     <div className="dropdown float-end">
-                                    <select className="form-select" aria-label="Default select example">
-                                    <option defaultValue>Select Graph Type</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
+                                    <select className="form-select" value={value.selectedValue} name='selectedValue' onChange={(e)=>handleInput(e)} aria-label="Default select example">
+                                    <option defaultValue disabled>Select Graph Type</option>
+                                    <option value="Pie">Pie</option>
+                                    <option value="Bar">Bar</option>
+                                    <option value="Line">Line</option>
                                     </select>
                                     </div>
                                     </div>
-                                    <div><Pie data={data} /></div>  
+                                    <div>
+                                    { renderGraph() }
+                                    </div>  
                                 </div>
                             </div>
                             </div>
                         </div>
                     </div>
-                </div>  
-                
-                <div className="row">
-                
-                </div>    
+                   
+                </div>      
             </div>
         )
     
